cf52d3ea01f00f149b9ca0121da0f002b0c73c85
Rafael carneiro <rafaelcdsilva46@gmail.com>
2026-06-14 12:59:27 -0300

    Versão inicial do projeto Oficina Mecânica

---
+++ src/main/java/com/example/Projeto_Oficina_Mecanica/ProjetoOficinaMecanicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_Oficina_Mecanica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoOficinaMecanicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoOficinaMecanicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projeto_Oficina_Mecanica/ProjetoOficinaMecanicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projeto_Oficina_Mecanica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoOficinaMecanicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
